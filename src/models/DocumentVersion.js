15bcfb0ad25899c6235407bd0a66bec16ff14f5d
daijiaqi <3547377842@qq.com>
2025-07-01 15:15:40 +0800

    fix: 修正versions表字长限制为正确表达

--- src/models/DocumentVersion.js
+++ src/models/DocumentVersion.js
@@ -19,7 +19,7 @@ const DocumentVersion = sequelize.define('DocumentVersion', {
     comment: '版本号'
   },
   content: {
-    type: DataTypes.LONGTEXT,
+    type: DataTypes.TEXT('long'),
     allowNull: false,
     comment: '该版本的内容'
   },
